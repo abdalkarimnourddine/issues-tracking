f5ded5f42bafbb6ab3161c68569c2c0d21539744
abd al karim Nourddine <abdalkarimnourddine@gmail.com>
2024-06-04 13:20:52 +0300

    fisrt commit

---
+++ app/NavBar.tsx
@@ -0,0 +1,33 @@
+'use client'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import React from 'react'
+import { AiFillBug } from "react-icons/ai";
+import classnames from 'classnames';
+
+const NavBar = () => {
+    const currentPath= usePathname()
+    const links=[
+        {label:'Dashboard',href:'/'},
+        {label:'issues' ,href:'/issues'}
+    ]
+  return (
+    <nav className='flex space-x-6 border-b-2 mb-5 px-5 h-14 items-center'>
+        <Link  href='/'><AiFillBug /></Link>
+    <ul className='flex space-x-6'>
+         {links.map(link => <Link key={link.href} 
+         className={classnames(
+            {'hover:text-zinc-500 transition-colors':true,
+             'text-zinc-900': link.href === currentPath,
+             'text-zinc-500': link.href !== currentPath,
+            }
+         )}
+         href={link.href}>{link.label}</Link> )}
+    </ul>
+    </nav>
+    
+  )
+}
+
+
+export default NavBar
