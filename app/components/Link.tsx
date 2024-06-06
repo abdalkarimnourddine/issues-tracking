1ceacb7742ff4d817bcc69ad80d48ba1c3806d54
abd al karim Nourddine <abdalkarimnourddine@gmail.com>
2024-06-06 16:57:24 +0300

    BUILD A STYLE LINK COMPONENT

---
+++ app/components/Link.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NextLink from 'next/link';
+import { Link as RadixLink } from '@radix-ui/themes';
+
+interface Porps {
+    href:string,
+    children:string
+}
+const Link = ({href,children}: Porps) => {
+  return (
+    <NextLink href={href} passHref legacyBehavior>
+        <RadixLink>{children}</RadixLink>
+    </NextLink>
+  )
+}
+
+export default Link
